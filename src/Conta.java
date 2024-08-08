a25859cb2ac053c97dce13dfe22618a0fdb0fa99
JulioCDias <juliozulu2008@gmail.com>
2024-08-08 15:12:04 -0300

    Ecapsulamneto, Metodos Getters e Setter

--- src/Conta.java
+++ src/Conta.java
@@ -1,17 +1,17 @@
 public class Conta {
     //Parametros de Ua classe
-    int agencia;
-    int numero;
-    Cliente titular; //Usando Referencia ao declarar que titular é um cliente
+    private int agencia;
+    private int numero;
+    private Cliente titular; //Usando Referencia ao declarar que titular é um cliente
     private double saldo;
 
     void depositar(double valor) { // Método de Classe, Void especifica que nao ha retorno
-        saldo += valor;
+        this.saldo += valor;
     }
 
     boolean sacar(double valor) {
-        if (valor <= saldo) {
-            saldo -= valor;
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
             System.out.println("Voçe relizou o saque de r$"+valor);
             return true;
         } else {
@@ -26,6 +26,36 @@ public class Conta {
             destino.depositar(valor);
         }
 
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        if (agencia > 0){
+            this.agencia = agencia;
+        }
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
     }
 }
 
--- src/TesteEncapsulamento.java
+++ src/TesteEncapsulamento.java
@@ -3,8 +3,6 @@ public class TesteEncapsulamento {
         Conta conta = new Conta();
         conta.depositar(1000);
         conta.sacar(1000);
-
-        conta.saldo = -300;
-        System.out.println(conta.saldo);
+        System.out.println(conta.getSaldo());
     }
 }
