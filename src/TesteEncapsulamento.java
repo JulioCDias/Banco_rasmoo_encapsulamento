b61f28fc9c62b2c33a51beee57bdeade783b9fae
JulioCDias <juliozulu2008@gmail.com>
2024-08-11 15:38:08 -0300

    Mais sobre metodos Getters e Setters, passagem por referencia

--- src/TesteEncapsulamento.java
+++ src/TesteEncapsulamento.java
@@ -1,8 +1,33 @@
 public class TesteEncapsulamento {
     public static void main(String[] args) {
+        //Cliente
+        Cliente cliente = new Cliente();
+        cliente.setNome("Julio");
+        cliente.setCpf("0123123123");
+        cliente.setProfissao("Desenvolvedor");
+        cliente.setSalario(5500);
+
+        //Conta
         Conta conta = new Conta();
         conta.depositar(1000);
         conta.sacar(1000);
+        conta.setAgencia(10);
+        conta.setTitular(cliente);
         System.out.println(conta.getSaldo());
+
+        //Pegando informações
+        System.out.println(conta.getTitular().getNome());
+        System.out.println(conta.getTitular().getProfissao());
+        System.out.println(conta.getTitular().getProfissao());
+        System.out.println(conta.getSaldo());
+
+        //Outra maneira
+        conta.setTitular(new Cliente());
+        conta.getTitular().setNome("Antonio");
+        conta.getTitular().setCpf("12233344");
+        conta.getTitular().setProfissao("P.O");
+        conta.getTitular().setSalario(25000);
+
+
     }
 }
